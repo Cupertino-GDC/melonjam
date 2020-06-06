d0d0b2e8f70be86a5712758e5c1adf2ebe12b67f
AMPNR <43621741+Nishant-Ray@users.noreply.github.com>
2020-06-06 09:12:58 -0700

    Added FAQ and footer.

--- js/primary.js
+++ js/primary.js
@@ -3,21 +3,33 @@ function scrollToElement(elementName, position) {
 	element.scrollIntoView({behavior: "smooth", block: position, inline: "nearest"});
 }
 
-function expandFAQ(element) {
+function scrollToTop() {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+}
 
-    if(element.style.height === "500px") {
-        element.style.height = "auto";
-        element.style.backgroundColor = "initial";
-    } else {
-        element.style.height = "500px";
-        element.style.backgroundColor = "rgba(255, 195, 103, 1.0)";
+function expandFAQ(i) {
+    var faq = document.getElementsByClassName('faq')[i];
+    var faqText = document.getElementsByClassName('faq-answer')[i];
+
+    if(faq.style.height === "400px") { // compress
+
+        faq.style.height = "auto";
+        faq.style.backgroundColor = "initial";
+        faqText.style.display = "none";
+
+    } else { // expand
+
+        faq.style.height = "400px";
+        faq.style.backgroundColor = "rgba(255, 195, 103, 1.0)";
+        faqText.style.display = "block";
+
     }
 
 }
 
 function enterHoverFAQ(element) {
 
-    if(element.style.height != "500px") {
+    if(element.style.height != "400px") {
         element.style.backgroundColor = "rgba(255, 195, 103, 0.5)";
     }
 
@@ -25,7 +37,7 @@ function enterHoverFAQ(element) {
 
 function exitHoverFAQ(element) {
 
-    if(element.style.height != "500px") {
+    if(element.style.height != "400px") {
         element.style.backgroundColor = "rgba(255, 195, 103, 0)";
     }
 
